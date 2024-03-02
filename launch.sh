6284a8e06820043a38c44670bd11d135233406a1
kev <yqlbu@bu.edu>
2024-03-02 15:16:09 +0800

    feat: persist desktop-specific configs

--- launch.sh
+++ launch.sh
@@ -2,3 +2,5 @@
 
 # Load the configuration
 waybar -c $HOME/.config/waybar/config.json -s $HOME/.config/waybar/style.css &
+waybar -c $HOME/.config/waybar/config.left.json -s $HOME/.config/waybar/style.css &
+waybar -c $HOME/.config/waybar/config.right.json -s $HOME/.config/waybar/style.css &
