6dceed53eb3842fa2e648a5a3a98a7874e9313aa
kev <yqlbu@bu.edu>
2024-04-21 23:22:21 +0800

    feat: add config.external.json

--- launch.sh
+++ launch.sh
@@ -2,3 +2,4 @@
 
 # Load the configuration
 waybar -c $HOME/.config/waybar/config.json -s $HOME/.config/waybar/style.css &
+waybar -c $HOME/.config/waybar/config.external.json -s $HOME/.config/waybar/style.css &
