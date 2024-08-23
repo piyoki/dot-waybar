643c6bc0595ec10529ada3b2654721fb8ccc8227
kev <31861128+piyoki@users.noreply.github.com>
2024-08-23 09:15:46 -0400

    refactor: reset desktop configs (#2)
    * feat: persist waybar configs for x1-carbon
    
    * patch: update font size
    
    * patch: remove the 6th workspace
    
    * feat: add battery support
    
    * feat: add backlight support
    
    * patch: update battery icon color scheme
    
    * patch: assign color to bluetooth icon
    
    * patch: rearrange position
    
    * patch: fix syntax
    
    * feat: map click event for network module
    
    * patch: update battery icon color
    
    * feat: add nix logo
    
    * patch: do not display temperature section
    
    * patch(style): apply formatting
    
    * patch: better icon theming
    
    * patch: adjust default margin
    
    * feat: add colorscheme
    
    * patch: config -> config.json
    
    * feat: add plugged icon
    
    * feat(module/battery): add profile-swtich
    
    * patch: fix module conflicts
    
    * feat: add custom-notification module
    
    * patch: adjust workspace active button color
    
    * patch: update notification configs
    
    * patch: demise notification module
    
    * patch(custom/logo): disable click
    
    * patch(style_: set clock bg to white
    
    * patch: udate default colorscheme
    
    * patch(battery): update state
    
    * patch: update icons
    
    * feat: add config.external.json
    
    * patch: update config.external.json
    
    * patch: fix deprecated settings
    
    * refactor: rework nixlogo launcher
    
    * patch: update custom/logo functionality (profile-switcher)
    
    * ci: add sync-downstream workflow
    
    (cherry picked from commit 6f9fe9f8c410e2509d058b8d5078a4beeaa40b3a)
    
    * ci(sync-downstream): apply tweaks
    
    (cherry picked from commit 13369e3897f434491ac5afc6f0141ca5af0fc665)
    
    * patch: update default config

--- launch.sh
+++ launch.sh
@@ -2,5 +2,3 @@
 
 # Load the configuration
 waybar -c $HOME/.config/waybar/config.json -s $HOME/.config/waybar/style.css &
-waybar -c $HOME/.config/waybar/config.left.json -s $HOME/.config/waybar/style.css &
-waybar -c $HOME/.config/waybar/config.right.json -s $HOME/.config/waybar/style.css &
